9c8fee9085d883a13a8ef7b59e3e3c8756f26df4
Brianna Grizzell <breegrizzell@gmail.com>
2019-11-20 23:26:27 -0800

    'intermediate'

---
+++ 2019-11-20/render-user.js
@@ -0,0 +1,18 @@
+function render(data,container){
+    container.innerHTML = ''; //Step 1: create an empty container
+    //Step 2: Check if the data is an array and if it has a length
+    if(Array.isArray(data) && data.length){
+        //Now we iterate through the array
+        data.forEach((user, index) => createNewElement({tag:'div', innerHTML:user, parent:container, className:'user', customAttributeValue:index}))
+    }
+
+    function createNewElement({tag, innerHTML,parent, className, customAttributeValue}){
+        //create a user element:
+        const userElement = document.createElement(tag);
+        userElement.innerHTML = innerHTML;
+        userElement.classList.add(className);
+        userElement.setAttribute('data-index', customAttributeValue);
+        parent.appendChild(userElement);
+    }
+
+}
