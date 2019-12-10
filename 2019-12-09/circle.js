8766c84c7e1c8cc9cb774d1fc788a3ac7e46959d
BP Mishra <bindhyeswari.mishra@gmail.com>
2019-12-10 11:51:01 -0800

    drag

--- 2019-12-09/circle.js
+++ 2019-12-09/circle.js
@@ -16,7 +16,6 @@ const createCircle = () => {
   parentNode.insertBefore(circle, refNode);
   return circle;
 };
-
 const createElement = props => {
   const {className, elemType, handlers, attributes = []} = props;
   const element = document.createElement(elemType);
@@ -26,48 +25,35 @@ const createElement = props => {
   });
   return element;
 };
+
 document.addEventListener('readystatechange', () => {
   if (document.readyState === 'interactive') {
-  
-    const circle = createCircle();
-    const {clientHeight, clientWidth} = circle;
-  
-    document.body.addEventListener('mouseup', (event) => {
-      // event.preventDefault(); // Read up on this ...
-      // fix the performance
-      // todo: The following for mousemove event
-      // todo: requestAnimationFrame
-      // todo: debounce
-      /*if (!event.ctrlKey) {
-        return;
-      }*/
-      console.log(event);
-      const {x, y} = event;
-      circle.setAttribute('style', `left: ${x - clientWidth / 2}px; top: ${y - clientHeight / 2}px;`);
-      console.log(`The mouse is at ... `, x, y);
-      // return {x,y};
+    const divElement = createCircle();
+    let isMouseDown = false,
+      height = divElement.clientHeight,
+      width = divElement.clientWidth;
+    document.body.addEventListener('mouseup', () => {
+      isMouseDown = false
     });
-    
-    let isDown = false;
-    
-    
-      document.body.addEventListener('mousedown', (event) => {
-        isDown = true;
-        console.log(event);
-        const {x1, y1} = event;
-        console.log('mousedown is active...');
-        console.log(`The mouse is at ... `, x1, y1);
-        
-        
-        document.body.addEventListener('mousemove', (event) => {
-          console.log('Tracking the cursor across the screen...')
-          
-        })
-      });
-      
-      // if(isDown === true) {
-      
-      // }
-    
+    document.body.addEventListener('mousedown', () => {
+      isMouseDown = true
+    });
+    document.body.addEventListener('mouseleave', () => {
+      isMouseDown = false
+    });
+    document.body.addEventListener('mousemove', (evt) => {
+      if (isMouseDown) {
+        console.log(isMouseDown);
+        const {clientX, clientY} = evt;
+        const isHeightChanged = height < clientY,
+          isWidthChanged = width < clientX;
+        if (isHeightChanged || isWidthChanged) {
+          isHeightChanged && (height = clientY);
+          isWidthChanged && (width = clientX);
+          divElement.setAttribute('style', `width: ${width}px; height: ${height}px`);
+        }
+      }
+    });
   }
 });
+
